ef9dc2e4a799bc8227d3ead1337a76ac9df68c46
Polymath1108 <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2026-08-02 20:18:51 +0530

    remoeved test files

--- check_farms.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "./lib/db";
-
-async function checkFarms() {
-  const res = await query("SELECT id, name FROM farms ORDER BY name");
-  console.log("TOTAL FARMS IN DB:", res.rows.length);
-  console.log("FARMS:", JSON.stringify(res.rows, null, 2));
-}
-
-checkFarms().catch(console.error);
